aab04634eb2fac9d284ba01ccc43c1b09e9170f7
kprotty <kbutcher6200@gmail.com>
2020-07-30 21:43:42 -0500

    test cases for task/task-batch

--- src/executor/thread.rs
+++ src/executor/thread.rs
@@ -5,6 +5,23 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+use core::{fmt, marker::PhantomPinned};
+
 /// TODO: documentation for a Thread
-#[derive(Debug)]
-pub struct Thread {}
+pub struct Thread {
+    _pinned: PhantomPinned,
+}
+
+impl fmt::Debug for Thread {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Thread").finish()
+    }
+}
+
+impl Default for Thread {
+    fn default() -> Self {
+        Self {
+            _pinned: PhantomPinned,
+        }
+    }
+}
---
+++ tests/executor_task.rs
@@ -0,0 +1,83 @@
+use core::{pin::Pin, ptr::NonNull};
+use yaar::executor::{Task, TaskBatch, TaskCallback, Thread};
+
+#[test]
+fn task_run() {
+    #[repr(C)]
+    struct TaskContext {
+        task: Task,
+        is_set: bool,
+    }
+
+    // have a callback which sets the context
+    extern "C" fn set_ctx(task: *mut Task, _: *const Thread) {
+        unsafe { (*(task as *mut TaskContext)).is_set = true };
+    }
+
+    // create an unset context
+    let mut ctx = TaskContext {
+        task: Task::from(set_ctx as TaskCallback),
+        is_set: false,
+    };
+
+    // call the callback which shuld set the context
+    unsafe {
+        let task = Pin::new_unchecked(&mut ctx.task);
+        task.run(Pin::new_unchecked(&Thread::default()));
+    }
+
+    assert!(ctx.is_set);
+}
+
+#[test]
+fn task_batch() {
+    extern "C" fn stub_fn(_: *mut Task, _: *const Thread) {}
+
+    let (mut t1, mut t2, mut t3) = (
+        Task::from(stub_fn as TaskCallback),
+        Task::from(stub_fn as TaskCallback),
+        Task::from(stub_fn as TaskCallback),
+    );
+
+    unsafe {
+        let mut batch = TaskBatch::new();
+        assert_eq!(batch.pop(), None);
+
+        // Test basic push and pop
+
+        batch.push(Pin::new_unchecked(&mut t1));
+        batch.push(Pin::new_unchecked(&mut t2));
+        assert_eq!(batch.pop(), NonNull::new(&mut t1));
+
+        batch.push(Pin::new_unchecked(&mut t3));
+        assert_eq!(batch.pop(), NonNull::new(&mut t2));
+        assert_eq!(batch.pop(), NonNull::new(&mut t3));
+        assert_eq!(batch.pop(), None);
+
+        // Test push from different sides of the batch
+
+        batch.push(Pin::new_unchecked(&mut t1));
+        batch.push_front(Pin::new_unchecked(&mut t2));
+        batch.push_back(Pin::new_unchecked(&mut t3));
+
+        assert_eq!(batch.pop(), NonNull::new(&mut t2));
+        assert_eq!(batch.pop(), NonNull::new(&mut t1));
+        assert_eq!(batch.pop(), NonNull::new(&mut t3));
+        assert_eq!(batch.pop(), None);
+
+        // Test different side push + pushing of entire batches
+
+        batch.push({
+            let mut b = TaskBatch::new();
+            b.push(Pin::new_unchecked(&mut t2));
+            b.push(Pin::new_unchecked(&mut t3));
+            b
+        });
+        batch.push_front(Pin::new_unchecked(&mut t1));
+
+        assert_eq!(batch.pop(), NonNull::new(&mut t1));
+        assert_eq!(batch.pop(), NonNull::new(&mut t2));
+        assert_eq!(batch.pop(), NonNull::new(&mut t3));
+        assert_eq!(batch.pop(), None);
+    }
+}
