177a4fc5905eec6ef2d01a2da72201cb69df1299
kprotty <kbutcher6200@gmail.com>
2021-10-04 14:27:43 -0500

    cleaning up project structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
-pub mod net;
-pub mod runtime;
-pub mod sync;
+#![warn(
+    // missing_debug_implementations,
+    // missing_docs,
+    rust_2018_idioms,
+    unreachable_pub
+)]
 
-pub use runtime::task;
-pub use runtime::task::spawn;
+#[cfg(feature = "net")]
+mod io;
+#[cfg(feature = "rt")]
+mod runtime;
+
+#[cfg(feature = "net")]
+pub mod net;
+#[cfg(feature = "sync")]
+pub mod sync;
+#[cfg(feature = "rt")]
+pub mod task;
--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,9 +1,1 @@
-mod builder;
-mod idle;
-pub(crate) mod io;
-pub(crate) mod pool;
-mod queue;
-pub mod task;
-mod worker;
 
-pub use builder::Builder;
