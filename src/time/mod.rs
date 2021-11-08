6261013a71582e0249073db6a7b58b2d7625c2f7
kprotty <kbutcher6200@gmail.com>
2021-11-08 16:41:31 -0600

    time: disable intervals for now

--- src/time/mod.rs
+++ src/time/mod.rs
@@ -1,10 +1,10 @@
 pub mod error;
-mod intervals;
+// mod intervals;
 pub(crate) mod queue;
 mod sleeps;
 mod timeouts;
 
-pub use intervals::{interval, interval_at, Interval};
+// pub use intervals::{interval, interval_at, Interval};
 pub use sleeps::{sleep, sleep_until, Sleep};
 pub use std::time::{Duration, Instant};
 pub use timeouts::{timeout, timeout_at, Timeout};
