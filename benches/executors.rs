90d504afd60c48b4a08cc998871d211aa57ea7fc
kprotty <kbutcher6200@gmail.com>
2021-12-15 12:24:28 -0600

    yield_many, spawn_many

--- benches/executors.rs
+++ benches/executors.rs
@@ -5,82 +5,55 @@ extern crate criterion;
 mod prelude;
 use prelude::*;
 
-use criterion::{black_box, Bencher, Criterion};
+use criterion::{Bencher, Criterion};
 
-fn bench_yield<E: BenchExecutor>(b: &mut Bencher, tasks: usize) {
+fn bench_yield_many<E: BenchExecutor>(b: &mut Bencher) {
+    const NUM_YIELDS: usize = 10_000;
+    const NUM_TASKS: usize = 100;
+
     b.iter_custom(|iters| {
-        E::record(async move {
-            let handles = (0..tasks).map(|_| {
+        E::record(iters, |wg| async move {
+            wg.reset(NUM_TASKS);
+
+            for _ in 0..NUM_TASKS {
+                let wg = wg.clone();
                 E::spawn(async move {
-                    for _ in 0..black_box(iters * 10) {
+                    for _ in 0..NUM_YIELDS {
                         tokio::task::yield_now().await;
                     }
-                })
-            });
-
-            for handle in handles.collect::<Vec<_>>() {
-                handle.await;
+                    wg.done();
+                });
             }
         })
     })
 }
 
-fn bench_spawn_join<E: BenchExecutor>(b: &mut Bencher, tasks: usize, concurrency: usize) {
+fn bench_spawn_many<E: BenchExecutor>(b: &mut Bencher) {
+    const NUM_TASKS: usize = 100_000;
+
     b.iter_custom(|iters| {
-        E::record(async move {
-            let handles = (0..concurrency).map(|_| {
+        E::record(iters, |wg| async move {
+            wg.reset(NUM_TASKS);
+
+            for _ in 0..NUM_TASKS {
+                let wg = wg.clone();
                 E::spawn(async move {
-                    let num_tasks = (tasks / concurrency) as u64 * iters;
-                    let handles = (0..num_tasks).map(|_| {
-                        E::spawn(async move {
-                            std::thread::yield_now();
-                        })
-                    });
-
-                    for handle in handles.collect::<Vec<_>>() {
-                        handle.await;
-                    }
-                })
-            });
-
-            for handle in handles.collect::<Vec<_>>() {
-                handle.await;
+                    wg.done();
+                });
             }
         })
     })
 }
 
-fn yield_now(c: &mut Criterion) {
-    let num_tasks = 100_000;
-    c.bench_function("tokio yield_now", |b| {
-        bench_yield::<TokioExecutor>(b, num_tasks)
-    });
-    c.bench_function("yaar yield_now", |b| {
-        bench_yield::<YaarExecutor>(b, num_tasks)
-    });
+fn yield_many(c: &mut Criterion) {
+    c.bench_function("tokio yield_many", |b| bench_yield_many::<TokioExecutor>(b));
+    c.bench_function("yaar yield_many", |b| bench_yield_many::<YaarExecutor>(b));
 }
 
-fn spawn_join_spmc(c: &mut Criterion) {
-    let concurrency = 1;
-    let num_tasks = 100_000;
-    c.bench_function("tokio spawn (spmc)", |b| {
-        bench_spawn_join::<TokioExecutor>(b, num_tasks, concurrency)
-    });
-    c.bench_function("yaar spawn (spmc)", |b| {
-        bench_spawn_join::<YaarExecutor>(b, num_tasks, concurrency)
-    });
+fn spawn_many(c: &mut Criterion) {
+    c.bench_function("tokio spawn_many", |b| bench_spawn_many::<TokioExecutor>(b));
+    c.bench_function("yaar spawn_many", |b| bench_spawn_many::<YaarExecutor>(b));
 }
 
-fn spawn_join_mpmc(c: &mut Criterion) {
-    let concurrency = num_cpus::get();
-    let num_tasks = 100_000;
-    c.bench_function("tokio spawn_join (mpmc)", |b| {
-        bench_spawn_join::<TokioExecutor>(b, num_tasks, concurrency)
-    });
-    c.bench_function("yaar spawn_join (mpmc)", |b| {
-        bench_spawn_join::<YaarExecutor>(b, num_tasks, concurrency)
-    });
-}
-
-criterion_group!(bench_executors, yield_now, spawn_join_spmc, spawn_join_mpmc);
+criterion_group!(bench_executors, yield_many, spawn_many);
 criterion_main!(bench_executors);
--- benches/prelude.rs
+++ benches/prelude.rs
@@ -2,9 +2,36 @@ use pin_project_lite::pin_project;
 use std::{
     future::Future,
     pin::Pin,
+    sync::atomic::{AtomicUsize, Ordering},
+    sync::Arc,
     task::{Context, Poll},
     time::{Duration, Instant},
 };
+use tokio::sync::Notify;
+
+#[derive(Default)]
+pub struct WaitGroup {
+    notify: Notify,
+    count: AtomicUsize,
+}
+
+impl WaitGroup {
+    pub fn reset(&self, count: usize) {
+        self.count.store(count, Ordering::Relaxed);
+    }
+
+    pub fn done(&self) {
+        if self.count.fetch_sub(1, Ordering::Release) == 1 {
+            self.notify.notify_one();
+        }
+    }
+
+    pub async fn ready(&self) {
+        while self.count.load(Ordering::Acquire) != 0 {
+            self.notify.notified().await;
+        }
+    }
+}
 
 pub trait BenchExecutor {
     type JoinHandle: Future<Output = ()> + Send + 'static;
@@ -13,11 +40,18 @@ pub trait BenchExecutor {
 
     fn block_on<F: Future<Output = ()>>(future: F);
 
-    fn record<F: Future<Output = ()>>(future: F) -> Duration {
+    fn record<F: Future<Output = ()>>(
+        iters: u64,
+        mut f: impl FnMut(Arc<WaitGroup>) -> F,
+    ) -> Duration {
         let mut elapsed = None;
         Self::block_on(async {
+            let wg = Arc::new(WaitGroup::default());
             let started = Instant::now();
-            future.await;
+            for _ in 0..iters {
+                f(wg.clone()).await;
+                wg.ready().await;
+            }
             elapsed = Some(started.elapsed());
         });
         elapsed.unwrap()
